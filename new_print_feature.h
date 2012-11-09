eb70d2e687fc633cd794222ab5f7cbfd05e081a8
parnurzeal <parnurzeal@gmail.com>
2012-11-09 13:36:16 +0900

    again add new print feature file

---
+++ new_print_feature.h
@@ -0,0 +1,13 @@
+#ifndef NEW_PRINT_FEATURE_H
+#define NEW_PRINT_FEATURE_H
+
+#include<iostream>
+
+class NewPrintFeature{
+ public:
+  NewPrintFeature(){}
+  void print(){ std::cout<<"Hello Branching Man!";}
+};
+
+
+#endif
